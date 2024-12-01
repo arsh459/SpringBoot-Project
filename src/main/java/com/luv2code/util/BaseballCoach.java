77eac91794bf90089273df1f818215ded929cdd4
arsh459 <arshgoyal459@gmail.com>
2024-12-01 21:54:48 +0530

    initial project

---
+++ src/main/java/com/luv2code/util/BaseballCoach.java
@@ -0,0 +1,12 @@
+package com.luv2code.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Base ball Coach";
+    }
+}
